cbc695dd2e5c2719db03edf14c727c0178c49033
Jake Backer <jbacker42@gmail.com>
2017-02-19 17:12:10 -0500

    Add Asset package, Add Player model, add movement, add physics

--- Assets/resources/Scripts/PlayerController.cs
+++ Assets/resources/Scripts/PlayerController.cs
@@ -4,35 +4,89 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    int speed {get; set;}
+
 	// Use this for initialization
 	void Start () {
-		
+        speed = 1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         UpdateVision();
+        UpdatePosition();
 	}
 
     void UpdateVision() {
-        Vector3 currentPos = this.transform.rotation.eulerAngles;
-        Vector3 newPos = currentPos;
 
-        newPos.x += -Input.GetAxis("Mouse X");
-        newPos.y += Input.GetAxis("Mouse Y");
+        GameObject camera = this.transform.FindChild("Camera").gameObject;
 
-        Debug.Log("Current Pos: " + currentPos.x);
-        Debug.Log("New Pos: " + newPos.x);
+        Vector3 currentCamPos = camera.transform.rotation.eulerAngles;
+        Vector3 currentPlayerPos = this.transform.rotation.eulerAngles;
+        Vector3 newCamPos = currentCamPos;
+        Vector3 newPlayerPos = currentPlayerPos;
 
+        newCamPos.x += -Input.GetAxis("Mouse X");
+        newPlayerPos.y += Input.GetAxis("Mouse Y");
 
-        // HACK: This is horrible... 80 is also a tottally random number
-        if ((newPos.x > 30 && newPos.x < 80)  || newPos.x < -180)
+        // HACK: This is horrible... 80 is also a totally random number
+        if ((newCamPos.x > 55 && newCamPos.x < 80)  || newCamPos.x < -180)
         {
-            newPos.x = currentPos.x;
+            newCamPos.x = currentCamPos.x;
         }
 
-        Debug.Log("Changed Pos: " + newPos.x);
+        camera.transform.rotation = Quaternion.Euler(newCamPos);
 
-        this.transform.rotation = Quaternion.Euler(newPos);
+        this.transform.rotation = Quaternion.Euler(newPlayerPos);
+
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+
+        if (camera.transform.rotation.eulerAngles.x > 80 && camera.transform.rotation.eulerAngles.x < 100)
+        {
+            Debug.LogWarning("STUCK PREVENTION ACTIVE!!!");
+            camera.transform.rotation = new Quaternion(0, 0, 0, 0);
+        }
+
+        // TODO: Add stuck prevention for going very fast upwards
+    }
+
+    bool hasJumped = false;
+    void UpdatePosition() {
+
+        if (Input.GetKey("w"))
+        {
+            transform.position += (transform.forward/10)*speed;
+        }
+
+        if (Input.GetKey("s"))
+        {
+            transform.position += ((-transform.forward)/15)*speed;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            transform.position += ((-transform.right)/10)*speed;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.position += (transform.right/10)*speed;
+        }
+
+        if (Input.GetKey("space") && !hasJumped)
+        {
+            transform.position += (transform.up / 3) * speed;
+            hasJumped = true;
+        }
+    }
+
+    void OnCollisionEnter(Collision coll) {
+        if (coll.gameObject.CompareTag("Block"))
+        {
+            if (hasJumped)
+            {
+                hasJumped = false;
+            }
+        }
     }
 }
