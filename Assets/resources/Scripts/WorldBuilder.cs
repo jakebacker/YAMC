b723f5c74aad19a8086a2ad9f0ff2825eb6be3eb
Jake Backer <jbacker42@gmail.com>
2017-03-22 15:12:19 -0400

    Add ability to generate an area of chunks

--- Assets/resources/Scripts/WorldBuilder.cs
+++ Assets/resources/Scripts/WorldBuilder.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class WorldBuilder : MonoBehaviour {
 
+    public const int CHUNK_SIZE = 8;
+
 	// Use this for initialization
 	void Start () {
-        GenerateChunk(new Vector2(-4, -4), 0);
-        GenerateChunk(new Vector2(4, 4), 0);
+        //GenerateChunk(new Vector2(-4, -4), 0);
+        //GenerateChunk(new Vector2(4, 4), 0);
+        GenerateArea(2, 2, new Vector2(0, 0));
         GenerateBlock(new Vector3(0,1,1), BlockType.GRASS_BLOCK);
+
 	}
 	
 	// Update is called once per frame
@@ -26,15 +30,15 @@ public class WorldBuilder : MonoBehaviour {
     }
 
     /// <summary>
-    /// Generates an 8 by 8 layer of blocks.
+    /// Generates an CHUNK_SIZE by CHUNK_SIZE layer of blocks.
     /// </summary>
     /// <param name="startingPos">The most negative corner of the layer (in x and z)</param>
     /// <param name="yHeight">The y height of the layer</param>
     /// <param name="type">The type of block to make the layer with</param>
     void GenerateLayer(Vector2 startingPos, int yHeight, string type) {
         // For now, ignore that yHeight has to change the blocks
-        for (int x=(int)startingPos.x; x<(int)startingPos.x+8; x++) {
-            for (int z = (int)startingPos.y; z < (int)startingPos.y + 8; z++) {
+        for (int x=(int)startingPos.x; x<(int)startingPos.x+CHUNK_SIZE; x++) {
+            for (int z = (int)startingPos.y; z < (int)startingPos.y + CHUNK_SIZE; z++) {
                 GenerateBlock(new Vector3(x, yHeight, z), type);
             }
         }
@@ -50,6 +54,24 @@ public class WorldBuilder : MonoBehaviour {
         GenerateLayer(startingPos, maxHeight, BlockType.GRASS_BLOCK);
         for (int i = maxHeight-1; i > -20; i--) { // 20 blocks tall for now
             GenerateLayer(startingPos, i, BlockType.DIRT_BLOCK);
+        }
+    }
+
+    /// <summary>
+    /// Generates an area of chunks. Use sparingly
+    /// </summary>
+    /// <param name="chunkWidth">Number of chunks wide</param>
+    /// <param name="chunkDepth">Number of chunks deep</param>
+    /// <param name="startingPos">Starting position</param>
+    void GenerateArea(int chunkWidth, int chunkDepth, Vector2 startingPos) {
+        int width = chunkWidth * CHUNK_SIZE;
+        int depth = chunkDepth * CHUNK_SIZE;
+        for (int i = 0; i < width; i+=8)
+        {
+            for (int ii = 0; ii < depth; ii+=8)
+            {
+                GenerateChunk(startingPos + new Vector2(i, ii), 0);
+            }
         }
     }
 }
